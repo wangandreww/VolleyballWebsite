9dbd703fd891c99826aeaadaf3b92280a971113f
wangandreww <59212728+wangandreww@users.noreply.github.com>
2021-12-09 12:35:26 -0500

    m

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,3 +4,5 @@ spinner = document.querySelector("#spinner");
 if (!sign) { angle = angle + 120; } else { angle = angle - 120; }
 spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
 }
+
+//#9daec8
